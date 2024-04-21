07729c8298f8aa7b053bb15d39d213515f33463a
Yusuf <mohdyusufhesam@gmail.com>
2024-04-21 11:52:40 +0530

    signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,9 @@
 import User from "../models/user.model.js"
 import bcrypt from "bcrypt"
+import jwt from 'jsonwebtoken'
+import { errorHandler } from "../utils/error.js";
+
+
 export const signup = async (req,res,next)=>{
     const {username, email,password} = req.body;
     const hashedPassword = await bcrypt.hash(password,10)
@@ -13,7 +17,31 @@ export const signup = async (req,res,next)=>{
     } catch (error) {
         next(error)
     }
-   
-
     
+}
+
+
+export const signin = async(req,res,next)=>{
+    const {email,password} = req.body;
+    try {
+        const validUser = await User.findOne({email});
+        if(!validUser){
+            return next(errorHandler(404,"user not found"));
+        }
+
+        const validPassword = await bcrypt.compare(password,validUser.password)
+        if(!validPassword){
+            return next(errorHandler(401,'wrong credentials'))
+        }
+
+        const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET)
+        const options ={httpOnly:true}//can set expiration here
+        const {password:pass, ...rest} =validUser._doc
+        res
+        .cookie('access_token',token,options)
+        .status(200)
+        .json(rest)
+    } catch (error) {
+        next(error)
+    }
 }
