5367821678bfafa88a4ff516c8cd2ec2a76deccf
Yusuf <mohdyusufhesam@gmail.com>
2024-06-23 15:21:54 +0530

    user update functionality added

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -20,9 +20,22 @@ const userSlice = createSlice({
         signInFailure:(state,action)=>{
             state.loading = false 
             state.error = action.payload
+        },
+        updateUserStart:(state)=>{
+            state.loading  = true;
+        },
+        updateUserSuccess:(state,action)=>{
+            state.currentUser = action.payload;
+            state.loading  = false;
+            state.error = null;
+        },
+        updateUserFailure:(state,action)=>{
+            state.error = action.payload;
+            state.loading = false;
         }
+
     }
 })
 
-export const {signInStart,signInFailure,signInSuccess} = userSlice.actions
+export const {signInStart,signInFailure,signInSuccess, updateUserStart,updateUserSuccess,updateUserFailure} = userSlice.actions
 export default userSlice.reducer
