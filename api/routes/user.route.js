c5fe6ae4a9055159c5b3faddace222162a8b6efc
Yusuf <mohdyusufhesam@gmail.com>
2024-07-11 16:19:51 +0530

    update listing api route added

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -7,5 +7,4 @@ router.get('/test',test)
 router.post('/update/:id',verifyToken,updateUser)
 router.delete('/delete/:id',verifyToken,deleteUser)
 router.get('/listings/:id',verifyToken,getUserListing)
-
 export default router
