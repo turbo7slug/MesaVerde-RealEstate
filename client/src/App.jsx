642958394385bd1e4984ca24e8f546a11b1494dc
Yusuf <mohdyusufhesam@gmail.com>
2024-07-12 06:27:43 +0530

    image slider added to listing page using swiper

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import SignIn from './pages/SignIn'
 import SignUp from "./pages/SignUp"
 import Profile from "./pages/Profile"
 import About from "./pages/About"
+import Listing from './pages/Listing'
 
 import Header from './components/Header'
 import PrivateRoute from './components/PrivateRoute'
@@ -20,6 +21,8 @@ function App() {
       <Route path="/sign-in" element ={<SignIn/>}/>
       <Route path="/sign-up" element ={<SignUp/>}/>
       <Route path="/about" element ={<About/>}/>
+      <Route path="/listing/:listingId" element ={<Listing />}/>
+
       <Route element={<PrivateRoute />}>
       <Route path="/profile" element ={<Profile/>}/>
       <Route path="/create-listing" element ={<CreateListing/>}/>
