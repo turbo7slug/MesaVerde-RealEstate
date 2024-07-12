80d6721b26251fa3214db939dcba541aab0ad694
Yusuf <mohdyusufhesam@gmail.com>
2024-07-13 01:44:46 +0530

    home and about page UI added

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-function About() {
-    return (
-        <>About</>
-    )
+export default function About() {
+  return (
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About MesaVerde</h1>
+      <p className='mb-4 text-slate-700'>Welcome to Mesaverde Real Estate, where finding your dream home is our top priority. Our mission is to provide a seamless and enjoyable home-buying experience, tailored to meet your unique needs and preferences.</p>
+      <p className='mb-4 text-slate-700'>
+      Our mission is to connect people with the homes of their dreams by leveraging technology, local expertise, and exceptional customer service. We are dedicated to offering comprehensive real estate solutions that cater to all your needs.
+      </p>
+      <p className='mb-4 text-slate-700'>We'd love to hear from you! If you have any questions, need assistance, or want to schedule a consultation, please don't hesitate to reach out.</p>
+    </div>
+  )
 }
-
-export default About
